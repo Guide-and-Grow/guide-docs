c1aa43f03af48026efcb08a036f6c7a23bc2201d
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-07-05 23:18:12 +0530

    docs : "add new docs python innerworking"

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,6 +31,7 @@ export default defineConfig({
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Introduction to Python', link: '/python/introduction' },
+						{ label: 'Python inner working', link: '/python/innerworking' },
 					],
 				},{
 
